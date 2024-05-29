b1909f763fa5d86337b9c3d31f8a8251ecc6275a
Luciano Fernández <lucianofernandez122@gmail.com>
2024-05-29 15:49:16 -0300

    student controller

---
+++ src/Web/Controllers/StudentController.cs
@@ -0,0 +1,83 @@
+﻿using ConsultaAlumnos.Application.Interfaces;
+using ConsultaAlumnos.Application.Models;
+using ConsultaAlumnos.Application.Models.Requests;
+using ConsultaAlumnos.Application.Services;
+using ConsultaAlumnos.Domain.Entities;
+using ConsultaAlumnos.Domain.Exceptions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConsultaAlumnos.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        private readonly IStudentService _studentService;
+        public StudentController (StudentService studentService)
+        {
+            _studentService = studentService;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<StudentDto> Get([FromRoute] int id)
+        {
+            try
+            {
+                return _studentService.GetById(id);
+            }
+            catch (NotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+
+        [HttpPost("[action]")]
+        public IActionResult Create([FromBody] StudentCreateRequest studentCreateRequest)
+        {
+            var newObj = _studentService.Create(studentCreateRequest);
+            return CreatedAtAction(nameof(Get), new { id = newObj.Id }, newObj);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            try
+            {
+                _studentService.Delete(id);
+                return NoContent();
+            }
+            catch (NotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+
+        [HttpGet("[action]")]
+        
+        public ActionResult<List<StudentDto>> GetAll()
+        {
+            return _studentService.GetAll();
+        }
+
+        [HttpGet("[action]")]
+        public ActionResult<List<Student>> GetAllFullData()
+        {
+            return _studentService.GetAllFullData();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update([FromRoute] int id, [FromBody] StudentUpdateRequest studentUpdateRequest)
+        {
+            try
+            {
+                _studentService.Update(id, studentUpdateRequest);
+                return NoContent();
+            }
+            catch (NotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+    }
+}

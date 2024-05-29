5b4c15daedec87b892654ce591081658f1bd27b9
patriciogazzera <patriciogazzera7@gmail.com>
2024-05-29 17:39:01 -0300

    Student Controller

--- src/Web/Controllers/StudentController.cs
+++ src/Web/Controllers/StudentController.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using ConsultaAlumnos.Application.Interfaces;
+using ConsultaAlumnos.Application.Models.Requests;
+using ConsultaAlumnos.Application.Models;
+using ConsultaAlumnos.Domain.Entities;
+using ConsultaAlumnos.Domain.Exceptions;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ConsultaAlumnos.Web.Controllers
@@ -7,5 +12,59 @@ namespace ConsultaAlumnos.Web.Controllers
     [ApiController]
     public class StudentController : ControllerBase
     {
+        private readonly IStudentService _studentService;
+        public StudentController(IStudentService studentService)
+        {
+            _studentService = studentService;
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] StudentCreateRequest studentCreateRequest)
+        {
+            var newObj = _studentService.Create(studentCreateRequest);
+            return CreatedAtAction(nameof(Get), new { id = newObj.Id }, newObj);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete([FromRoute] int id)
+        {
+            try
+            {
+                _studentService.Delete(id);
+                return NoContent();
+            }
+            catch (NotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<StudentDto> Get([FromRoute] int id)
+        {
+            try
+            {
+                return _studentService.GetById(id);
+            }
+            catch (NotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+
+        [HttpGet]
+        public ActionResult<List<StudentDto>> GetAll()
+        {
+            return _studentService.GetAll();
+        }
+
+        [HttpGet("[action]")]
+        public ActionResult<List<Student>> GetAllFullData()
+        {
+            return _studentService.GetAllFullData();
+        }
+
+
     }
+
 }
